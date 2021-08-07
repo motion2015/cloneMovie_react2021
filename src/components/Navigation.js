ffdb074b4c38049af811c3de4f2d17b0f8252872
nosidemean <nosidemean@gmail.com>
2021-08-07 23:15:59 +0900

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navigation.css'
+const Navigation = () => {
+  return (
+    <div className="nav">
+      <Link to="/">Home</Link>
+      <Link to={{ pathname:'/about', state: { fromNavigation: true }}}>About</Link>
+    </div>
+  )
+}
+
+export default Navigation
---
+++ src/routes/Detail.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Detail extends Component {
+  componentDidMount() {
+    const {location, history} = this.props
+    if (location.state === undefined) {
+      history.push('/');
+    }
+  }
+  render() {
+    const {location} = this.props
+    if( location.state) {
+      return (
+        <div className="movie__deatils">{location.state.title}</div>
+      )
+    } else {
+      return null;
+    }
+  }
+}
+
